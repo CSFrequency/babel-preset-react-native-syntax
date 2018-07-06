95f38802fcfbf0da2665a3934d3d48c51c60cb85
Chris Bianca <chris@csfrequency.com>
2018-07-06 10:28:39 +0100

    Update to support Babel 7

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-module.exports = {
-  plugins: [
-	"syntax-async-functions",
-    "syntax-class-properties",
-    "syntax-flow",
-    "syntax-jsx",
-    "syntax-object-rest-spread",
-    "syntax-trailing-function-commas",
-  ]
-}
+module.exports = function() {
+  return {
+    plugins: [
+      "syntax-async-functions",
+      "syntax-class-properties",
+      "syntax-flow",
+      "syntax-jsx",
+      "syntax-object-rest-spread",
+      "syntax-trailing-function-commas",
+    ]
+  };
+};
